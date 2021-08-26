f0e20f9c592f42294c03314d3ae748a0a208b053
Eduardo <eduardo@engebione.local>
2021-08-26 15:53:32 -0300

    finalizando sistema

--- Dio.Series/Program.cs
+++ Dio.Series/Program.cs
@@ -6,8 +6,35 @@ namespace Dio.Series
     {
         static void Main(string[] args)
         {
+            string opcao = OpcaoDoUsuario();
+
+            while(opcao != "X"){
+
+                
+
             
-            
+                OpcaoDoUsuario();
+            }
+            Console.WriteLine("Programa encerrado");
+        }
+
+
+        private static string OpcaoDoUsuario(){
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir uma nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("6 - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine("");
+
+            string opcaoDesejada = Console.ReadLine().ToUpper();
+            Console.ReadLine();
+            return opcaoDesejada;
+
         }
     }
 }
